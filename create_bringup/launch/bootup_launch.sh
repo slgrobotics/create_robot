36e34f4c1a9b25e8364a00c43bf5a559442bc75d
Sergei Grichine <msghub@hotmail.com>
2025-01-09 12:41:27 -0600

    Update bootup_launch.sh

--- create_bringup/launch/bootup_launch.sh
+++ create_bringup/launch/bootup_launch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#
+# See https://github.com/slgrobotics/robots_bringup/tree/main/Docs/Create1
+#
+# This file goes to ~/launch folder on Create 1 Turtlebot Raspberry Pi
+# Use it to start the on-board part of the robot.
+# On the Desktop machine type:
+#     cd ~/robot_ws; colcon build; ros2 launch articubot_one turtle.launch.py
+#
+
 cd /home/ros/launch
 source /opt/ros/jazzy/setup.bash
 source /home/ros/robot_ws/install/setup.bash
