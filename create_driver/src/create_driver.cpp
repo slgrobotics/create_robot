0783ae55b492a1267c0b46307c65a59e02d1449e
Sergei Grichine <msghub@hotmail.com>
2025-02-04 19:54:05 -0600

    Update create_driver.cpp
    Charge percentage should be in 0 to 1 range, not 0 to 100%

--- create_driver/src/create_driver.cpp
+++ create_driver/src/create_driver.cpp
@@ -535,23 +535,26 @@ void CreateDriver::publishBatteryInfo()
   charge_ratio_pub_->publish(float32_msg_);
   */
 
-  /* https://docs.ros2.org/foxy/api/sensor_msgs/msg/BatteryState.html
+  /* https://docs.ros.org/en/jazzy/p/sensor_msgs/interfaces/msg/BatteryState.html
 
-  float voltage
-  float temperature
-  float current
-  float charge
-  float capacity
-  float design_capacity
-  float percentage
-  uint8 power_supply_status
-  uint8 power_supply_health
-  uint8 power_supply_technology
-  boolean present
-  float[] cell_voltage
-  float[] cell_temperature
-  string location
-  string serial_number
+  float32 voltage          # Voltage in Volts (Mandatory)
+  float32 temperature      # Temperature in Degrees Celsius (If unmeasured NaN)
+  float32 current          # Negative when discharging (A)  (If unmeasured NaN)
+  float32 charge           # Current charge in Ah  (If unmeasured NaN)
+  float32 capacity         # Capacity in Ah (last full capacity)  (If unmeasured NaN)
+  float32 design_capacity  # Capacity in Ah (design capacity)  (If unmeasured NaN)
+  float32 percentage       # Charge percentage on 0 to 1 range  (If unmeasured NaN)
+  uint8   power_supply_status     # The charging status as reported. Values defined above
+  uint8   power_supply_health     # The battery health metric. Values defined above
+  uint8   power_supply_technology # The battery chemistry. Values defined above
+  bool    present          # True if the battery is present
+
+  float32[] cell_voltage   # An array of individual cell voltages for each cell in the pack
+                          # If individual voltages unknown but number of cells known set each to NaN
+  float32[] cell_temperature # An array of individual cell temperatures for each cell in the pack
+                            # If individual temperatures unknown but number of cells known set each to NaN
+  string location          # The location into which the battery is inserted. (slot number or plug)
+  string serial_number     # The best approximation of the battery serial number
   */
 
   battery_state_msg_.header.stamp = now();
@@ -562,7 +565,7 @@ void CreateDriver::publishBatteryInfo()
   battery_state_msg_.charge = robot_->getBatteryCharge();
   battery_state_msg_.capacity = robot_->getBatteryCapacity();
   battery_state_msg_.design_capacity = robot_->getBatteryCapacity();
-  battery_state_msg_.percentage = 100.0f * robot_->getBatteryCharge() / robot_->getBatteryCapacity();
+  battery_state_msg_.percentage = robot_->getBatteryCharge() / robot_->getBatteryCapacity();
   battery_state_msg_.power_supply_status = battery_state_msg_.POWER_SUPPLY_STATUS_UNKNOWN; // 0  (POWER_SUPPLY_STATUS_NOT_CHARGING=3)
   battery_state_msg_.power_supply_health = battery_state_msg_.POWER_SUPPLY_HEALTH_GOOD; // 1
   battery_state_msg_.power_supply_technology = battery_state_msg_.POWER_SUPPLY_TECHNOLOGY_LION; // 2
